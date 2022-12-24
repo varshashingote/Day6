62f4cf754834ba2f556ba6ffe0e6ed80f7c11190
varshashingote <120667268+varshashingote@users.noreply.github.com>
2022-12-24 19:07:21 +0530

    Add files via upload

---
+++ primeNo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "Enter the Number :" n
+for((i=2; i<=$n/2; i++))
+do
+  a=$(( n%i ))
+  if [ $a -eq 0 ]
+  then
+    echo "$n is not a prime number."
+    exit 0
+  fi
+done
+echo "$n is a prime number."
